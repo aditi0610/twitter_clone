f9946d60fb6232ecbf3a54efd54b37c0593be849
Aditi Parihar <aditiparihar06@gmail.com>
2025-02-09 13:43:28 +0530

    Add basic CRUD actions to TweetsController

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,45 @@
 class TweetsController < ApplicationController
     def index
+        @tweets = Tweet.all 
     end
+
 
     def new
+        @tweet = Tweet.new 
     end
+
 
     def create
+        @tweet = Tweet.new(tweet_params)
+        if @tweet.save
+            redirect_to @tweet
+        else
+            render 'new'
+            
+        end
     end
 
+
     def show
+        @tweet = Tweet.find(params[:id])
     end
+
      
-    def destroy
+    def edit
+        @tweet = Tweet.find(params[:id])
     end
+
+
+    def destroy
+        @tweet = Tweet.find(params[:id])
+    end
+
+
+    private
+
+    def tweet_params
+        params.require(:tweet).permit(:tweet)
+    end
+    
+
 end
