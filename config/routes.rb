1fe001cab3e1886ac2f592e4624cd010fd8a6d08
Aditi Parihar <aditiparihar06@gmail.com>
2025-02-09 11:59:53 +0530

    create tweet mmodel and set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/tweets", to: "tweets#index", as: "tweets"    # List all tweets
+  # get "/tweets", to: "tweets#index", as: "tweets"    # List all tweets
+  # get "tweets/new", to: "tweets#new", as: "new_tweet"   # Form for new tweet
+  # get "/tweets/:id", to: "tweets#show", as: "tweet"      # Display a single tweet
+
+  resources :tweets
 end
