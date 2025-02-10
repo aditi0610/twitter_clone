1ddb3470369026507a66124381e8d545fcb3b64b
Aditi Parihar <aditiparihar06@gmail.com>
2025-02-10 10:47:41 +0530

    flash notice and alert

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
     }
   else
     config.action_controller.perform_caching = false
-
+ 
     config.cache_store = :null_store
   end
 
@@ -53,5 +53,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
 end
