7f9583c3b4cb541c61caafae78a59b01c77698c2
Aditi Parihar <aditiparihar06@gmail.com>
2025-02-14 11:21:59 +0530

    Set up Devise authentication and generate users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get "/tweets", to: "tweets#index", as: "tweets"    # List all tweets
   # get "tweets/new", to: "tweets#new", as: "new_tweet"   # Form for new tweet
