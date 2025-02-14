386d2c9bee75e469cf3af40bed314b56bb3656e6
Aditi Parihar <aditiparihar06@gmail.com>
2025-02-14 15:58:59 +0530

    add columns name and username.

--- db/migrate/20250214095528_add_fields_to_users.rb
+++ db/migrate/20250214095528_add_fields_to_users.rb
@@ -1,4 +1,7 @@
 class AddFieldsToUsers < ActiveRecord::Migration[5.1]
   def change
+    add_column :users, :name, :string
+    add_column :users, :username, :string
+    add_index :users, :username, unique: true #Creates a unique index on the username column to ensure no two users have the same username.
   end
 end
