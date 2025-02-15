b8e298ce2000a6809fbc778f053c84bb4c5c8527
Aditi Parihar <aditiparihar06@gmail.com>
2025-02-15 18:21:21 +0530

    generate migration for add user id to tweets

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20250214095528) do
+ActiveRecord::Schema.define(version: 20250215124740) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "tweet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
