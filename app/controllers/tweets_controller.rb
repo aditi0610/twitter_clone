2b6cfccd6627579d2ef69e89e1a72b66851ffae6
Aditi Parihar <aditiparihar06@gmail.com>
2025-02-15 18:40:35 +0530

    create update and destroy action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -36,9 +36,21 @@
         # @tweet = Tweet.find(params[:id])
     end
 
+    def update
+        # @tweet = Tweet.find(params[:id]) 
+    
+        if @tweet.update(tweet_params)
+            redirect_to @tweet
+        else
+            render 'edit'
+        end
+    end
 
     def destroy
         # @tweet = Tweet.find(params[:id])
+         @tweet.destroy
+
+        redirect_to tweets_path
     end
 
 
